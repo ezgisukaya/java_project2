9b90f174c32f6e7b9a2ce901b6f2fa640c261614
ezgi su <mavvicam@gmail.com>
2024-09-03 17:08:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayi gir ve tavsiye al:");
+
+        int value= input.nextInt(); // Kullanıcıdan bir üst sınır değeri al
+        switch (value){
+            case 1:
+                System.out.println("u look so sweet");
+            break;
+
+            case 2:
+               System.out.println("have a nice day ");
+            break;
+
+            case 3:
+                System.out.println("u should drink water ");
+                break;
+
+            default:
+                //hicbiri ise burası
+            break;
+
+        }
+
+        input.close(); // Scanner'ı kapat
+    }
+}
